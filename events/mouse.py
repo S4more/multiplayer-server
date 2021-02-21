bfcb7dc93824cb8987a954b92a3319271b3ebb2b
S4more <guimsc@hotmail.com>
2021-02-20 21:41:51 -0500

    event mouse fix

--- events/mouse.py
+++ events/mouse.py
@@ -4,17 +4,15 @@ from .Events import Event
 
 class Mouse(Event):
     type = "mouse_move"
-    mouse_cords = {}
 
     def handle(self, player, cords):
-        self.mouse_cords[player] = cords
         player.mouse_cords = cords
 
 
-    def response(self):
-        return json.dumps({"type": self.type, "cords": [self.mouse_cords[ws] for ws in self.mouse_cords]})
+    def response(self, users):
+        return json.dumps({"type": self.type, "cords": [user.mouse_cords for user in users]})
 
     async def notify(self, users):
         if users:
-            message = self.response()
+            message = self.response(users)
             await asyncio.wait([users[user].ws.send(message) for user in users])
