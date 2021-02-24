ffe0bf6136589101a2db2d4c9d507ed378ff92ee
S4more <guimsc@hotmail.com>
2021-02-24 13:18:53 -0500

    Initial typescript structure.

--- README.md
+++
@@ -1,3 +0,0 @@
-# multiplayer-server
-
-This is a project made by three dawson students. And this message is just a test for a webhook
--- user.py
+++
@@ -1,5 +0,0 @@
-class Player:
-    def __init__(self, websocket):
-        self.ws = websocket
-        self.mouse_cords = [0, 0]
-        self.player_cords = [0, 0]
